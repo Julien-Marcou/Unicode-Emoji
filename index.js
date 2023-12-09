0a6edb899956a2207997ef0fd2e5237f257d5df7
Julien Marcou <julien.marcou.dev@gmail.com>
2023-12-09 18:13:10 +0100

    Update to Unicode Emoji v15.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import UNICODE_EMOJI from './unicode-emoji.js';
 
-const UNICODE_EMOJI_VERSIONS = ['0.6', '0.7', '1.0', '2.0', '3.0', '4.0', '5.0', '11.0', '12.0', '12.1', '13.0', '13.1', '14.0', '15.0'];
+const UNICODE_EMOJI_VERSIONS = ['0.6', '0.7', '1.0', '2.0', '3.0', '4.0', '5.0', '11.0', '12.0', '12.1', '13.0', '13.1', '14.0', '15.0', '15.1'];
 
 const emojiVariations = new Map();
 
